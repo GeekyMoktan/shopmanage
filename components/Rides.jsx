53a4cf74cf054877226123483def901e5bc8a827
GeekyMoktan <39195080+GeekyMoktan@users.noreply.github.com>
2022-11-24 21:32:00 +0545

    1 file modified

--- components/Rides.jsx
+++ components/Rides.jsx
@@ -3,87 +3,74 @@ import * as React from 'react';
 function Rides(){
   return(
     
-<div class="w-full p-4 text-center border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 ">
+    <div class=" pt-10 sm:pt-5 w-full min-h-screen gap-4 flex-wrap flex justify-center items-center">
 
+ 
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+
+    <img class="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" />
+    <div class="p-2">
+  
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+
+ 
+    </div>
+
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-purple-600 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+   
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+  
+    <img class="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" />
+    <div class="p-2">
+    
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+ 
+   
+    </div>
+ 
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-sky-500 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+  
+
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+ 
+    <img class="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" />
+    <div class="p-2">
+ 
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+   
+    
+    </div>
+ 
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-green-500 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+   
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+
+    <img class="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" />
+    <div class="p-2">
+    
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
   
     
-    <div class="items-center justify-center space-y-4 sm:flex flex-wrap gap-y-3.5 sm:space-y-0 sm:space-x-4">
-
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-
-    
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
     </div>
-
-    
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
+  
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-yellow-500 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
     </div>
+  </div>
+  
+  
+  
 
-    
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
-    </div>
-
-    
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
-    </div>
-
-    
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
-    </div>
-
-    
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
-    </div>
-
-    
-    <div class="bg-white border border-gray-200 rounded-lg shadow-md flex flex-col items-center pb-10">
-        
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div class=" flex mt-4 space-x-3 md:mt-6">
-            
-        </div>
-    </div>
-
-      
-    </div>
 </div>
-
   )
 }
 
