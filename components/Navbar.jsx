5b114120cc5e1ce7e4bf183357278fc8329205e9
GeekyMoktan <39195080+GeekyMoktan@users.noreply.github.com>
2022-11-25 16:18:52 +0545

    1 file modified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,11 +28,21 @@ function ShopIcon() {
 }
 
 function Signing() {
-  const [on]
+  const [on,setOn] = useState(false)
+
+  const changeOn = () => {
+    setOn(!on)
+  }
   return (
-    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 mr-1 border border-blue-500 text-xs hover:border-transparent rounded">
+    <Fragment>
+    <button onClick={changeOn}
+    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 mr-1 border border-blue-500 text-xs hover:border-transparent rounded">
       sign in
     </button>
+    {
+      on === true ? <SignUp /> : null 
+    }
+    </Fragment>
   );
 }
 
