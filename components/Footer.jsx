9914513f2ab051e9be30821d73bc1b0a78d34eba
GeekyMoktan <39195080+GeekyMoktan@users.noreply.github.com>
2022-11-28 21:28:27 +0545

    1 file modified

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
   return (
     
 <footer class="mt-10 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">BOOKING 101</a>. All Rights Reserved.
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a  class="hover:underline">BOOKING 101</a>. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
